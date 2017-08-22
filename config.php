8713440cf819ff18a0e99ed40befd7fab0826d2e
531devv <531devv@gmail.com>
2017-08-22 02:59:24 +0200

    v0.0.3

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 531
+ * Date: 2017-08-21
+ * Time: 20:23
+ */
+
+return array(
+    'ts3' => array(
+        'ip' => '127.0.0.1',
+        'query_port' => '10011',
+        'user' => 'elo',
+        'password' => 'fmMr792e',
+        'port' => '9987'
+    ),
+);
